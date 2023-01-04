e9807dcfd10770988420c522258ae58355730648
redimer <redimer97@gmail.com>
2023-01-04 16:55:15 +0700

    Работа с файлами

--- main.cpp
+++ main.cpp
@@ -1,60 +1,53 @@
 ﻿#include <iostream> // input output stream
 
+#include <fstream>
 
-#include "math.h" // Чистый СИ либо Ваши
+#pragma warning(disable : 4996)
 
-//#include <cmath> // C++
-
-using namespace std; // Пространство имён
-
-namespace RED
-{
-	int a;
-}
-
-
-int a = 20; // Глобальные переменные (есть стартовые значения)
+// # метакоманды
 
 
 int main()
 {
 
-	cout << a;
+	// "r" - Чтение (Только смотреть, но не изм) 
+	// "w" - Запись (Редактировать) 
+	// "e" - Выполнять
 
-	RED::a;
+//Условная компиляция
+#ifdef RED  // Если у меня в свойствах есть указаное ключ слово
+	//То код будет выполнятся, иначе нет
+	freopen("input.txt", "r", stdin); // Перенаправили консольный input на файл 
+	freopen("output.txt", "w", stdout); // Вам больше не нужна консоль
+#endif
 
-	// console input
-	//std::cerr
-	int a = 40;	int b;
-
-
-	cout << a;
-
-
-	//cout << b;
-
-	//std::cin >> a;
-	//std::cin >> b;
-
-	cin >> a >> b;
-
-	int c;
-
-	c = a + b;
-
-	//std::cout << b;
-	
-
-	//char ch = "a";
-	char ch = 'a';
-
-	//std::cout << ' '; // Одинарные ковычки - это символ
-	//std::cout << "-----";// Двойные ковычки - это массив символов (строка)
-	//std::cout << c;
-
-	std::cout << ' ' << "-----" << c;
+	//FILE* IN;
+	//FILE* OUT;
+	//freopen_s(&IN,"input.txt", "r", stdin); // Их нету в GNU g++
+	//freopen_s(&OUT, "output.txt", "w", stdout);
 
 
+	//std::ifstream my_file_in("input.txt"); // Ищем файл input.txt в корне проекта
+	////std::ifstream my_file_in("C://file1/file2/input.txt"); // Ищем файл input.txt в корне проекта
+
+	int a, b, A, c;
+
+	int d = c + a++;
+	++a;
+	bool(a) && bool(b);
+	bool(a) & bool(b); // Теперь одно и тоже
+
+
+	//my_file_in >> a >> b; // Пробелы табуляции 
+	std::cin >> a >> b;
+
+	////и переносы строк нужны как разделители
+
+	//std::ofstream my_output("output.txt");
+
+	std::cout << a + b;
+
+	//my_output << a + b;
 
 
 
