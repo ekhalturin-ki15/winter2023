738195b6775cacc23e7b581412cf24b50d62b598
redimer <redimer97@gmail.com>
2023-01-04 15:00:15 +0700

    Типы данных

---
+++ main.cpp
@@ -0,0 +1,73 @@
+
+
+// ctrl + alt + L
+
+int main()
+{
+	//
+	//Тип   Имя   
+	int Chislo1 = 123543453;  // 4 байт
+
+	Chislo1 = 4366; // Оператор присваивания = // Pascal :=  // Java = // Python = // JS = // R <-
+	// Сравнение на равенство    ==
+
+	Chislo1 = 0x110e; // Шестна запись
+
+	//int a = -2^31 ... 2^31-1
+
+	long long ll; // 8 байт
+
+	short sh; // 2 байт
+
+
+	float f = 1.5; // 4 байт   3 знака
+
+	float ff = 545e-2;
+
+	float fff = 545e2;
+
+	double llf = 1.4363466; // 8 байт      6 знаков - 9 знаков
+	long double lllf; // 10 байт
+
+
+	char Simvol1; // 1 символ 1 байт  // -128    127   как int был знаковый
+
+	bool bl; // 1 бит   
+
+	bool bl2;
+
+	//__int16 i; // short
+	//__int32 t; GNU G++ такого типа нет
+
+	unsigned int ui = -124234;
+
+	const int A = 324324; // Не редактируемая
+
+	//Арифметические операции  +   -   *    /       %     
+
+	60 % 7;// = 60 - (60 / 7) * 7
+
+
+	int aa = 5;
+	int bb = 6;
+
+	int cc = aa - bb;
+
+
+
+
+
+
+
+
+	//A = 435435;
+
+
+
+
+	//char* chm; массив из СИ
+
+	//std::string  c++
+
+
+}
