33ff6d6a536f343fd8c2d2021d309109d923c304
redimer <redimer97@gmail.com>
2023-01-04 17:02:11 +0700

    Условия

--- main.cpp
+++ main.cpp
@@ -21,33 +21,30 @@ int main()
 	freopen("output.txt", "w", stdout); // Вам больше не нужна консоль
 #endif
 
-	//FILE* IN;
-	//FILE* OUT;
-	//freopen_s(&IN,"input.txt", "r", stdin); // Их нету в GNU g++
-	//freopen_s(&OUT, "output.txt", "w", stdout);
-
-
-	//std::ifstream my_file_in("input.txt"); // Ищем файл input.txt в корне проекта
-	////std::ifstream my_file_in("C://file1/file2/input.txt"); // Ищем файл input.txt в корне проекта
-
-	int a, b, A, c;
-
-	int d = c + a++;
-	++a;
-	bool(a) && bool(b);
-	bool(a) & bool(b); // Теперь одно и тоже
-
-
-	//my_file_in >> a >> b; // Пробелы табуляции 
+	int a, b;
 	std::cin >> a >> b;
 
-	////и переносы строк нужны как разделители
+	if ((a < 5) ^ (b < 5))
+	{
+		std::cout << "УРА";
 
-	//std::ofstream my_output("output.txt");
 
-	std::cout << a + b;
+	}
+	else
+	{
 
-	//my_output << a + b;
+		std::cout << "НЕ УРА";
+	}
+
+
+
+
+
+
+
+
+
+	
 
 
 
