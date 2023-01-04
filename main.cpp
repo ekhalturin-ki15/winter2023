f22643bb1c3115baca5986b9a519a51c548971cd
redimer <redimer97@gmail.com>
2023-01-04 22:05:50 +0700

    Решение задачи про округление

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream> // input output stream
-
 #include <fstream>
+
+#include <iomanip>
 
 #pragma warning(disable : 4996)
 
@@ -9,11 +10,28 @@
 
 int main()
 {
+	//unsigned long long a; // 0   2^64 - 1
 
-	//Вечерняя пара
+	//unsigned long long b = (a / 2) * 2 + 2;
+
+
+	//long long c = 2 - (a % 2); // -2^63  ..  2^63 - 1 
+	//c = 2 -  a & 1;
+	//a += c;
+
+	//b / 100;
+
+	//b % 100;
+
+	double a, b;
+
+	
 
 
 
+	scanf("%lf %lf", &a, &b); // Ввод данных  (& оператор взятие адреса переменной)
+
+	std::cout << std::setprecision(9) << std::fixed << a / b;
 
 
 }
