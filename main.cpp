985dd71f48054047b8b4807feafabbf839f348b0
redimer <redimer97@gmail.com>
2023-01-05 14:48:44 +0700

    Указатели и адреса

--- main.cpp
+++ main.cpp
@@ -17,51 +17,51 @@ int main()
 { // Новая область видимости
 	//freopen("input.txt", "r", stdin);
 
+	
 	ifstream in("input.txt");
 	ofstream out("output.txt");
 
-	int ab, bb, a;
-	in >> a;
 
-//	goto BB;
 
-	switch (a) // Перечислимая переменная
+	unsigned long long a = 10000;
+	//unsigned long long b(10000);
+
+
+	unsigned long long* ptr_a; // Указатель хранит адрес
+
+	ptr_a = (&a); // Получить адрес
+
+
+	unsigned long long b = (*&a);
+
+	out << b <<" ";
+
+	out << ptr_a << " ";
+
+	*ptr_a; // Обращение по указанному адресу
+
+
+	(*ptr_a) = 777;
+
+	a = 999;
+
+	out << a << "\n";
+
+
 	{
-	default:
+		int a = 10;
 
-	case 10: // Меток  
+		out << a << "\n";
 
-		out << "A";
+		a = 1000;
 
-		break;
+		out << (*ptr_a) << "\n";
 
-	case 12:
-		out << "B";
-		break;
+	}
+
+
 	
-	}
-	int b;
-
-	/*if (a % 2 == 0)
-	{
-		b = 0;
-		
-	}
-	else
-	{
-		b = 1;
-	}*/
-
-	b = (a % 2 == 0) ? 0 : 1;
-
-	//(false) ? cout << "dfs" : cout << "3254324";
-
-
-
-
-	out << b;
-
-
+	//(*0x534) = 324325;
 
 
 }
