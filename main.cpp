dc8230cf7a19ed211d8fe458d9cba74be5322c6c
redimer <redimer97@gmail.com>
2023-01-09 14:27:14 +0700

    Повтор

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 #include <vector>
 
+
+#include <set>
+
 using namespace std;
 
 
@@ -21,9 +24,57 @@ class R
 };
 
 
+int f(int a, int b)
+{
+
+	return 3;
+}
 
 
-int main()
+
+template <typename T>
+int f(float b, T c)
+{
+	c <<= 10;
+
+	return 8;
+}
+
+
+struct Q
+{
+	int a;
+
+
+
+	int E(int q)
+	{
+
+
+		return 10;
+	}
+
+
+
+
+};
+
+
+
+int qq(int step)
+{
+	if (step >= 10)
+	{
+		
+		return 0;
+
+	}
+	cout << step << " ";
+	qq(step + 1);
+	cout << step << " ";
+}
+
+int main(int n, char** str)
 {
 #ifdef _DEBUG
 	ios_base::sync_with_stdio(0); cin.tie(0);
@@ -32,10 +83,16 @@ int main()
 	freopen_s(&OUT, "output.txt", "w", stdout);
 #endif
 
+	Q qqq;
+
+	qqq.E(10);
 
 	int q[1000]; // [0  1000)
 
 	int* w = nullptr; // nullptr указатель в никуда
+
+	qq(0);
+
 
 	if (w)
 	{
@@ -62,21 +119,11 @@ int main()
 
 	v[1];
 
-
 	v.resize(10);
 
 	v.assign(100, -1);
 
 
 
-
-
-
-
-
-
-
-
-
-
+	return 0;
 }
